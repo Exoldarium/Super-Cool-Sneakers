54787fc043acd8a9922a6be702acabde7fcc2ff3
Dusan <shandoo91@gmail.com>
2022-12-13 16:12:04 +0100

    removed web vitals

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { productInfo } from './data';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,4 +8,3 @@ root.render(
     <App product={productInfo}/>
 );
 
-reportWebVitals();
